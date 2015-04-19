583ca1d0107f7656beed3545143f027c0f2170b8
jonschlinkert <jon.schlinkert@sellside.com>
2015-04-19 03:22:21 -0400

    add sync/async tests

--- test.js
+++ test.js
@@ -7,21 +7,25 @@
 
 'use strict';
 
-var path = require('path');
+/* deps:mocha jquery */
+require('should');
 var handlebars = require('handlebars');
 var Template = require('template');
-var template = new Template();
 var resolve = require('./');
-require('should');
-
-var expected = 'node_modules/jquery/index.js';
+var template;
 
 describe('resolve helper', function () {
-  it('should work as a Template helper (works with any engine):', function () {
+  beforeEach(function () {
+    template = new Template();
+    template.engine('*', require('engine-lodash'));
+  });
+
+  it('should work as an async helper with Template:', function () {
     var str = '<script src="<%= resolve("jquery") %>"></script>';
+
     template.asyncHelper('resolve', function (fp, cb) {
-      resolve(fp, function (err, res) {
-        cb(null, res.cwd);
+      resolve(fp, function (err, resolved) {
+        cb(null, resolved.main);
       });
     });
 
@@ -31,10 +35,20 @@ describe('resolve helper', function () {
     });
   });
 
+  it('should work as a sync helper with Template:', function () {
+    var str = '<script src="<%= resolve("jquery").main %>"></script>';
+    template.helper('resolve', resolve.sync);
+
+    template.render(str, function (err, content) {
+      if (err) console.log(err);
+      content.should.equal('<script src="node_modules/jquery/dist/jquery.js"></script>');
+    });
+  });
+
   it('should work as a handlebars helper:', function () {
-    var str = '{{resolve "jquery" cwd="test"}}';
+    var str = '{{resolve "jquery"}}';
     handlebars.registerHelper('resolve', function (fp) {
-      return resolve.sync(fp).cwd;
+      return resolve.sync(fp).main;
     });
 
     handlebars.compile(str)().should.equal('node_modules/jquery/dist/jquery.js');
